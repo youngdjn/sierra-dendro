d865af1ac517463578099c7eb0754d1ac4959ecd
Andrew Latimer <amlatimer@ucdavis.edu>
2020-01-13 12:59:54 -0800

    load, merge data. prepare for analysis by keeping important columns and standardizing explanatory variables

--- scripts/analysis-climate-sensitivity/hierarchical-sensitivity-analysis.R
+++ scripts/analysis-climate-sensitivity/hierarchical-sensitivity-analysis.R
@@ -10,38 +10,60 @@ years <- read.csv("./data/compiled-for-analysis/years.csv")
 plots <- read.csv("./data/compiled-for-analysis/plots.csv")
 trees <- read.csv("./data/compiled-for-analysis/trees.csv")
 
-# Using tree.id, merge tree-level info (species, plot.id, dbh, elev, cluster, voronoi.area) into yearly increment data set
-d_long <- left_join(years, trees[,c("tree.id", "species", "plot.id", "x", "y", "elev", "cluster", "voronoi.area", "radius", "radius.external", "AR1")], by="tree.id", )
+# Keep only relevant rows of tree data
+trees_select <- trees[ , c("tree.id", "species", "plot.id", "x", "y", "elev", "cluster", "voronoi.area", "radius", "radius.external")]
 
-# Using plot.id, merge plot-level info (ppt.norm, tmean.norm, x.plot, y.plot, rad.tot,rad.03, rad.06, voronoi.area.mean) into yearly increment data set. 
-d_long <- merge(d_long, plots[, c("plot.id", "ppt.norm", "tmean.norm", "x.plot", "y.plot", "rad.tot", "rad.03", "rad.06", "voronoi.area.mean", "n.trees")], by="plot.id")
-head(d_long)
+# Merge
+d_long <- left_join(trees_select, years, by="tree.id") %>% 
+    left_join(plots, by="plot.id")
 
-## Subset data for analysis. 
-# Check length of each tree record
-tree_record_length_table <- table(d_long$tree.id[!is.na(d_long$raw_width)])
+#### Subset data for analysis ####
+
+# Filter by number of data points per tree 
+tree_record_length_table <- table(d_long$tree.id[!is.na(d_long$raw_width)]) # Check length of each tree record
 hist(tree_record_length_table, main="Histogram of tree-ring record lengths")
 min_record_length <- 30 # drop trees with shorter record than this number of years
-# Assemble data
-d <- filter(d_long, tree.id %in% names(tree_record_length_table)[tree_record_length_table >= min_record_length])
+d <- filter(d_long, tree.id %in% names(tree_record_length_table)[tree_record_length_table >= min_record_length]) # Drop trees with less than minimum length
+
+# Filter by year -- drop all data before a given date 
+# For now, keep all years back to 1956
+#length(unique(d$tree.id))
+#d <- filter(d, year > 1955) # keep last 53 years (50 plus 3 lags)
+
+# Filter by species -- keep just the big 3
+d <- filter(d, species %in% c("ABCO", "PIPO", "PSME")) # keep the 3 common species
 dim(d)
 length(unique(d$tree.id))
-d <- filter(d, year > 1961) # keep last 53 years (50 plus 3 lags)
-d <- filter(d, species == "PSME") # keep just one species
-#d <- filter(d, species %in% c("ABCO", "PIPO", "PSME")) # keep the 3 common species
-dim(d)
-length(unique(d$tree.id))
+
+# Save file 
+#write.csv(d, "./working-data/tree-ring-data-long.csv")
+
+
+
+#### Set up data for analysis #### 
+
+#d <- read.csv("./working-data/tree-ring-data-long.csv")
 
 # Keep only the necessary columns
-d <- dplyr::select(d, cluster, plot.id, tree.id, species, year, rwi, raw_width, voronoi.area, radius.external, ppt.norm, tmean.norm, rad.tot, starts_with("ppt.z"), starts_with("tmean.z"))
+d <- dplyr::select(d, cluster.x, cluster.y, plot.id, tree.id, species, year, rwi, raw_width, ba, bai, voronoi.area, radius.external, ppt.norm, tmean.norm, rad.tot, starts_with("ppt.z"), starts_with("tmean.z"))
 head(d)
 
+#### NOTE: Get TMAX as well -- Derek will extract
+
+#### NOTE: Get spline values as well 
+
+#### NOTE: ALSO get lag1 rwi values
+
+# Create lag1 variable for RWI
+#rwi_wide <- select(d, c(tree.id, rwi, year)) %>%
+#            reshape(idvar = "tree.id", v.names="rwi", timevar="year", direction="wide")
+#rwi_mat <- as.matrix(select(rwi_wide, -tree.id))
+#rwi_wide_lag1 <- as.data.frame(t(apply(rwi_mat, 1, lag)))
+#rwi_wide_lag1$tree.id <- rwi_wide$tree.id
+#rwi_long_lag1 <- reshape(rwi_wide_lag1, direction="long", idvar="tree.id", timevar="year", varying=list(1:ncol(rwi_mat)), sep=".")
 
 # Center and scale unstandardized columns 
 d <- mutate(d, ppt.norm.std = scale(ppt.norm), tmean.norm.std = scale(tmean.norm), year.std = scale(year), rad.tot.std = scale(rad.tot), voronoi.area.std = scale(voronoi.area))
 
-# Create a new variable that represents "within-plot long-term growth rate z-score": how much faster or slower does each tree grow than the average for its plot? 
-d$plot_mean_rw <- with(d, ave(raw_width, plot.id, FUN=function(x) mean(x, na.rm=T)))
-d$tree_mean_rw <- with(d, ave(raw_width, tree.id, FUN=function(x) mean(x, na.rm=T)))
-d$plot_sd_rw <- with(d, ave(tree_mean_rw, plot.id, FUN=function(x) sd(x, na.rm=T)))
-d <- mutate(d, tree.growth.z = (tree_mean_rw - plot_mean_rw)/plot_sd_rw)
+#### Specify model ####
+
