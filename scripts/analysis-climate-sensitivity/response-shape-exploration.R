3e0047f2333ec4dab2f288f1d8d50f3b522e09fa
Andrew Latimer <amlatimer@ucdavis.edu>
2020-04-07 11:05:14 -0700

    take long analysis of ppt growth response form and put into a new file, to keep the modeling script cleaner.

--- scripts/analysis-climate-sensitivity/response-shape-exploration.R
+++ scripts/analysis-climate-sensitivity/response-shape-exploration.R
@@ -1,123 +1,14 @@
-# This script: 
-# 1) combines Derek's processed data into a long-form data frame for analysis;
-# 2) filters the data based on species, year, and number of data points per tree; 
-
-library(dplyr)
-library(readr)
-library(ggplot2)
-library(brms)
-library(rstan)
-
-#### Load and merge data ####
-years <- read.csv("./data/compiled-for-analysis/years.csv")
-plots <- read.csv("./data/compiled-for-analysis/plots.csv")
-trees <- read.csv("./data/compiled-for-analysis/trees.csv")
-
-# Keep only relevant rows of tree data
-trees_select <- trees[ , c("tree.id", "species", "plot.id", "x", "y", "elev", "cluster", "voronoi.area", "radius", "radius.external")]
-
-# Merge
-d_long <- left_join(trees_select, years, by="tree.id") %>% 
-    left_join(plots, by="plot.id")
-
-#### Subset data for analysis ####
-
-# Filter by number of data points per tree 
-tree_record_length_table <- table(d_long$tree.id[!is.na(d_long$raw_width)]) # Check length of each tree record
-hist(tree_record_length_table, main="Histogram of tree-ring record lengths")
-min_record_length <- 30 # drop trees with shorter record than this number of years
-d <- filter(d_long, tree.id %in% names(tree_record_length_table)[tree_record_length_table >= min_record_length]) # Drop trees with less than minimum length
-
-# Filter by year -- drop all data before a given date 
-# For now, keep all years back to 1956
-#length(unique(d$tree.id))
-#d <- filter(d, year > 1955) # keep last 53 years (50 plus 3 lags)
-
-# Filter by species -- keep just the big 3
-d <- filter(d, species %in% c("ABCO", "PIPO", "PSME")) # keep the 3 common species
-dim(d)
-length(unique(d$tree.id))
-
-# Save file 
-#write.csv(d, "./working-data/tree-ring-data-long.csv")
+#### Exploring the response shapes of tree growth to ppt variation ####
 
 
 
-#### Set up data for analysis #### 
+# It's kind of weird that a lot of studies use a linear growth~precip slope as an index of growth sensitivity. It's simple and easy to intepret, but it's probably quite wrong. It seems biologically likely that there's an underlying non-linear response function relating tree growth to moisture availability. This is presumably saturating. It may even appear unimodal, but where it does, this is probably because other factors associated with very high precipitation depress growth: increased snowpack, which shortens effective growing season, or decreased solar radiation during early parts of the growing season. 
 
-d <- read.csv("./working-data/tree-ring-data-long.csv")
+# In the EDA in this script, I tried to figure out how strong this nonlinearity is, and whether the nonlinearity appears within individual trees or only across trees or plots. The short answer: individual trees' growth responses are generally nonlinear an saturating, and might be represented OK by a piecewise linear regression, but not a linear regression.
 
-# Keep only the necessary columns
-d <- dplyr::select(d, cluster.x, cluster.y, plot.id, tree.id, species, year, rwi, raw_width, ba, bai, voronoi.area, radius.external, ppt.norm, tmean.norm, rad.tot, ppt, tmean, starts_with("ppt.z"), starts_with("tmean.z"))
-head(d)
+# The observed saturating response shape presumably varies among genotypes and even among phenotypes for the same genotype (or even individual over time). The hypotheses this study seeks to address can all be framed in terms of this response shape and its relationship to patterns of moisture availability across the region. 
 
-#### NOTE: Get TMAX as well -- Derek will extract
-
-#### NOTE: Get spline values as well 
-
-#### NOTE: ALSO get lag1 rwi values
-
-# Create lag1 variable for RWI
-#rwi_wide <- select(d, c(tree.id, rwi, year)) %>%
-#            reshape(idvar = "tree.id", v.names="rwi", timevar="year", direction="wide")
-#rwi_mat <- as.matrix(select(rwi_wide, -tree.id))
-#rwi_wide_lag1 <- as.data.frame(t(apply(rwi_mat, 1, lag)))
-#rwi_wide_lag1$tree.id <- rwi_wide$tree.id
-#rwi_long_lag1 <- reshape(rwi_wide_lag1, direction="long", idvar="tree.id", timevar="year", varying=list(1:ncol(rwi_mat)), sep=".") # Broken -- doesn't preserve year
-
-# Center and scale unstandardized columns 
-d <- mutate(d, ppt.norm.std = scale(ppt.norm), tmean.norm.std = scale(tmean.norm), year.std = scale(year), rad.tot.std = scale(rad.tot), voronoi.area.std = scale(voronoi.area))
-
-
-
-
-#### Test run a mixed model on the data in stan 
-
-model.path <- ("./scripts/analysis-climate-sensitivity/lmm_random_slopes.stan")
-d_psme <- dplyr::filter(d, species=="PSME")
-d_psme <- d_psme[!is.na(d_psme$rwi) & !is.na(d_psme$ppt.z),]
-d_psme$plot_index <- match(d_psme$plot.id, unique(d_psme$plot.id))
-d_psme_plot <- summarise(group_by(d_psme, plot.id), raw_width_mean = mean(raw_width, na.rm=T), ppt.norm = mean(ppt.norm, na.rm=T))
-  
-d_psme$plot_index <- match(d_psme$plot.id, unique(d_psme$plot.id))
-stan.data <- list(N=nrow(d_psme), N_groups = max(d_psme$plot_index), y = d_psme$rwi, x = d_psme$ppt.z, group_index = d_psme$plot_index)
-
-m <- stan(file=model.path, data=stan.data)
-print(m)
-check_hmc_diagnostics(m)
-stan_dens(m, pars = c("sigma_y", "sigma_a", "sigma_b")
-
-#### Specify model ####
-
-# Simplest model at tree level has one one weather variable (ppt.z) and its short-term lag (ppt.z1), one autoregressive term (lag(growth, 1)). 
-# Simplest model at plot level -- explaining variation in mean growth, and in sensitivity of growth to precipitation, among plots, using long-term normal precipitation. 
-
-
-
-
-
-
-mod.form <- brmsformula(rwi ~ ppt.z*ppt.norm.std + ppt.z1 + lag(rwi, 1) + (1 + lag(rwi, 1)| plot.id))
-
-brms.data <- dplyr::filter(d, species=="PSME")
-
-prior <- c(set_prior("normal(0, 2)", class = "Intercept", coef = ""), set_prior("normal(0, 2)", class = "b", coef = ""), set_prior("cauchy(0, 2)", class = "sd", coef = ""))
-
-m0 <- brm(mod.form, data=brms.data, prior=prior, family=gaussian(), chains=3, cores=3, iter=100)
-
-summary(m0)
-
-
-
-#######################################
-#### Data visualization side-track ####
-
-# Underlying idea of using linear response shape is that there is an underlying non-linear resopnse function relating tree growth to moisture availability. This is presumably saturating. It may even appear unimodal, but where it does, this is probably because other factors associated with very high precipitation depress growth: these are likely increased snowpack, which shortens effective growing season, or decreased solar radiation during early parts of the growing season. 
-
-# This likely saturating response shape presumably varies among genotypes and even among phenotypes for the same genotype (or even individual over time). 
-
-# The hypotheses this study seeks to address can all be framed in terms of this response shape and its relationship to patterns of moisture availability across the region. 
-# 1) As a species nears its range edge, we expect an increase in sensitivity. This prediction relies on the assumtion that there is a relatively consistent response function for each tree species and that this response function steepens toward lower levels of moisture availability. In drier regions, the trees express greater sensitivity because they are exposed to lower rainfall. If these assumptions hold roughly true, we would expect species approaching their range limit to exhibit a more rapid increase in sensitivity, because their (species-level) response function drops more sharply as moisture declines. 
+# 1) As a species nears its range edge, we expect an increase in sensitivity. This prediction relies on the assumption that there is a relatively consistent response function for each tree species and that this response function steepens toward lower levels of moisture availability. In drier regions, the trees express greater sensitivity because they are exposed to lower rainfall. If these assumptions hold roughly true, we would expect species approaching their range limit to exhibit a more rapid increase in sensitivity, because their (species-level) response function drops more sharply as moisture declines. 
 
 # Note on time scales. It's important to keep in mind that the response function we're looking at is relatively slow -- it's linked to an annual growth cycle, and because of exogenous and endogenous lags, its time scale is effectively longer than 1 year. So we are not talking about instantaneous rates of photosynthesis, or even daily averages, but rather the cumulative effects of weather and other conditions over time spans of more than a year. Why does this longer time-scale matter? Because it means that the "performance" metric reflects photosynthesis during early season when moisture is available, later season when it is low or even u avaibable, and during dormant winter months when metabolic costs and snow or frost damage imposes costs, and further the influence of all these factors in at least the previous year. 
 
@@ -167,7 +58,7 @@ ggplot(d[d$cluster.y=="Sierra",], aes(ppt, rwi, group=tree.id)) + facet_wrap(~pl
 ggplot(d[d$cluster.y=="Sierra",], aes(ppt.z, rwi, group=tree.id, color=tree.id)) + facet_wrap(~plot.id, nrow=3)   + theme_bw()  +  theme(legend.position="none")+ geom_smooth(method=loess, se=FALSE, fullrange=FALSE) #+ geom_point(cex=0.5) + scale_color_gradient2(midpoint=mean(d$year, na.rm=T), low="blue", mid="white", high="red", space ="Lab" )
 #linear
 ggplot(d[d$cluster.y=="Sierra",], aes(ppt.z, rwi, group=tree.id, color=tree.id)) + facet_wrap(~plot.id, nrow=3)   + theme_bw()  +  theme(legend.position="none")+ geom_smooth(method=lm, se=FALSE, fullrange=FALSE)
-# Interesting consistency here is apparent breakpoint for a lot of trees and plots around 1000 mm precip or a bit less, with some trees showing a second acceleration at high end -- this appears to be real, not driven by a single high-leverage year. Further, if we color the points by year, this reveals excellent interspersion of ppt and decade; this is a longer-term pattern not driven by an extreme. Quite interesting - what accounts for the kink in some plots but not others? Are there differences in snowpack? 
+# Interesting consistency here is apparent breakpoint for a lot of trees and plots around 1000 mm precip or a bit less, with some trees showing a second acceleration at high end -- this appears to be real, not driven by a single high-leverage year. Further, if we color the points by year, this reveals excellent interspersion of ppt and decade; this is a longer-term pattern not driven by a single extreme or recent change. Quite interesting - what accounts for the kink in some plots but not others? Are there differences in snowpack? 
 
 # Does the pattern look qualitatively similar if we look at raw growth rates? 
 ggplot(d[d$cluster.y=="Sierra",], aes(ppt, raw_width, group=tree.id, color=year)) + facet_wrap(~plot.id, nrow=3)   + theme_bw()  +  theme(legend.position="none")+ geom_smooth(method=loess, se=FALSE, fullrange=FALSE) 
@@ -209,6 +100,8 @@ ggplot(d_all[d_all$cluster.y=="Plumas",], aes(ppt, rwi, color=species)) + facet_
 ggplot(d_all[d_all$cluster.y=="Plumas",], aes(ppt, raw_width, color=species)) + facet_wrap(~plot.id, nrow=3)   + theme_bw()  + geom_smooth(method=loess, se=T, fullrange=FALSE)  +  theme(legend.position="none")
 ggplot(d_all, aes(ppt, raw_width, color=species))   + theme_bw()  + geom_smooth(method=loess, se=T, fullrange=FALSE)  + facet_wrap(~cluster.y)
 
+ggplot(d_all, aes(ppt, rwi, color=species))   + theme_bw()  + geom_smooth(method=loess, se=T, fullrange=FALSE)  + facet_wrap(~cluster.y)
+
 ggplot(d, aes(ppt, raw_width, color=cluster.y))    + theme_bw()  + geom_smooth(method=loess, se=T, fullrange=FALSE)  
 ggplot(d, aes(ppt, rwi, color=cluster.y))    + theme_bw()  + geom_smooth(method=loess, se=T, fullrange=FALSE)  
 ggplot(d, aes(ppt, rwi, color=cluster.x))    + theme_bw()  + geom_smooth(method=loess, se=T, fullrange=FALSE)  
@@ -218,6 +111,6 @@ ggplot(d, aes(ppt.z, rwi, color=cluster.x))    + theme_bw()  + geom_smooth(metho
 ggplot(d, aes(ppt.z, rwi, color=cluster.x))    + theme_bw()  + geom_smooth(method=lm, formula = y~poly(x, 1), se=T, fullrange=FALSE) 
 # Steeper slope relative to local variation in lower-elevation plots. NL and SL are very similar, nearly identical. NH is lease sensitive, and even shows decline at high z-scores; SH is intermediate. Could this suggest that sensitivity increases to the same degree at the range limit in the south, as at the elevational local range limits? And further that adapation is not enough to buffer the response to drier extremes? 
 
-# Conclusion on linearity for the purposes of model structure. We should not model the response shape as linear. It's not, even at the level of individual trees, ALTHOUGH sometimes it is -- apparently in low-elevation, wet sites where precipitation is less limiting and there is also little snowpack. But linearity at the tree level is the exception. To generalize and over-simplify, for most trees, there is a breakpoint below which sensitivity increases. Below this point, generally we don't have enough data to determine if this decline is accelerating, and mostly it seems like it could be reasonable to represent it as linear. Above this breakpoint is a shallower slope that's also possibly linear. In many trees, especially in higher-rainfall areas, but really in all the clusters, there's a second breakpoint beyond which the response of growth to ppt either re-accelerates, or alternatively becomes negative. The negative trend is most prevalent in Tahoe, though you also see it in Plumas. My guess is that this negative trend at high precipitation will be primarily in higher-elevation or colder sites, and is driven by snowpack. 
+# Conclusion on linearity for the purposes of model structure. We should not model the response shape as linear. It's not, even at the level of individual trees, ALTHOUGH sometimes it is -- apparently in low-elevation, wet sites where precipitation is less limiting and there is also little snowpack. But linearity at the tree level is the exception. To generalize: for most trees, there is a breakpoint below which sensitivity increases. Below this point, generally we don't have enough data to determine if this decline is accelerating, and mostly it seems like it could be reasonable to represent it as linear. Above this breakpoint is a shallower slope that's also possibly linear. In many trees, especially in higher-rainfall areas, but really in all the clusters, there's a second breakpoint beyond which the response of growth to ppt either re-accelerates, or alternatively becomes negative. The negative trend is most prevalent in Tahoe, though you also see it in Plumas. My guess is that this negative trend at high precipitation will be primarily in higher-elevation or colder sites, and is driven by snowpack. 
 
 # Alternative modeling methods: a) piecewise linear with a breakpoint; b) quadradic; c) basis spline. I don't know how to do any of these in a hierarchical way but the piecewise linear and quadratic should be not too complicated. 
